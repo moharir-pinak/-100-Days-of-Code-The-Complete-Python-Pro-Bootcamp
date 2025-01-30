e83f1e710fa9bb62f85418b596cdf87994c12dcb
pinakmoharir <pinakmoharir12@gmail.com>
2025-01-30 12:30:01 +0530

    day5

---
+++ Day5/HighestScore.py
@@ -0,0 +1,17 @@
+scores = [180,124,165,173,189,169,146]
+
+# 1st method 
+# highest = scores[0]
+
+# for score in scores :
+#     if score > highest :
+#         highest = score 
+#     else :
+#         highest = highest
+# print (highest)
+
+# 2nd method 
+
+highest = max(scores)
+print(highest)
+    
