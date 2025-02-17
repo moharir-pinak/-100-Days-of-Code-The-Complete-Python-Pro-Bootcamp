f7b3614d00ed13e48a72bb9b2fbd308116fc521e
pinakmoharir <pinakmoharir12@gmail.com>
2025-02-17 13:20:29 +0530

    Day17

---
+++ Day17/question_model.py
@@ -0,0 +1,5 @@
+# Task Number 1 =========================>   Creating a question class    <=========================
+class Question :
+    def __init__(self , question, q_answer) :
+        self.text = question
+        self.answer = q_answer
